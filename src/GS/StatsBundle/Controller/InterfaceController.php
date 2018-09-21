beb16b2b3127cf07240139d196393f5a104347d8
Unknown <alexperez@wanadoo.fr>
2018-09-21 18:17:13 +0200

    Charts design quasi ended

--- src/GS/StatsBundle/Controller/InterfaceController.php
+++ src/GS/StatsBundle/Controller/InterfaceController.php
@@ -17,4 +17,16 @@ class InterfaceController extends Controller
             'repo' => $repo
         ));
     }
+
+    public function analysisAction()
+    {
+        $repo = $this->get('gs_stats.repository');
+        $todayDate = new \DateTime("now", new \DateTimeZone("EUROPE/Paris"));
+        // $repo->setEndDate($todayDate->modify('-50 day'));
+        // $repo->setStartDate($todayDate->modify('-100 day'));
+
+        return $this->render('GSStatsBundle:Interface:analysis.html.twig', array(
+            'repo' => $repo
+        ));
+    }
 }
