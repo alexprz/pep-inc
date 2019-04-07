b5f8cf755c3a0f1a853597c5844b250628a6d96d
Alexandre <alexperez@wanadoo.fr>
2019-04-07 15:06:54 +0200

    Working login in both prod and dev

--- onstart.sh
+++ onstart.sh
@@ -3,3 +3,9 @@ php symfony/bin/console assetic:dump --env=dev --no-debug
 php symfony/bin/console cache:clear --env=prod
 php symfony/bin/console assetic:dump --env=prod --no-debug
 php symfony/bin/console assets:install --symlink
+
+# php bin/console cache:clear --env=dev
+# php bin/console assetic:dump --env=dev --no-debug
+# php bin/console cache:clear --env=prod
+# php bin/console assetic:dump --env=prod --no-debug
+# php bin/console assets:install --symlink
